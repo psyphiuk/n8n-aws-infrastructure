1d74310100122aeb7ef3c288a6feeab7f4a6960f
Simon Filtness <simon_filtness@yahoo.co.uk>
2025-06-02 15:57:57 +0100

    Update proxy settings

--- aws/bootstrap.sh
+++ aws/bootstrap.sh
@@ -87,6 +87,9 @@ server {
     proxy_set_header Host $host;
     proxy_set_header X-Real-IP $remote_addr;
     proxy_buffering off;
+    proxy_http_version 1.1;
+    proxy_set_header   Upgrade $http_upgrade;
+    proxy_set_header   Connection $connection_upgrade;
   }
 
   # ACME challenge location for Let’s Encrypt
